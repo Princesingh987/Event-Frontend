d8816f3f08305e4578bb5dc4fcf9173601cf9b53
Princesingh987 <168814139+Princesingh987@users.noreply.github.com>
2025-08-16 22:58:54 +0530

    cdvfv

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // src/config.js
 // Backend API base URL (production)
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
-
+console.log("Backend URL:", API_BASE_URL);
 export default API_BASE_URL;
+
